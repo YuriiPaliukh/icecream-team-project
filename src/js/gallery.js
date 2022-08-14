fa6102a45a9eadf43bae346c1837e47d85b601a3
YuriiPaliukh <revcrew.palyuh60@gmail.com>
2022-08-14 20:03:06 +0200

    ф

--- src/js/gallery.js
+++
@@ -1,7 +0,0 @@
-import SimpleLightbox from "simplelightbox/dist/simple-lightbox.min.css";
-import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
-
-var lightbox = new SimpleLightbox('.gallery__list a', { 
-    captionsData: 'alt',
-    captionsDelay: 250,
- });
